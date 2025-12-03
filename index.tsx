7912dfc020d47047f4ad41e81e79761c6a57053e
beatoalex <126612004+beatoalex@users.noreply.github.com>
2025-12-03 17:04:34 -0300

    inedx.html feito

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // Importa o arquivo CSS principal
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
